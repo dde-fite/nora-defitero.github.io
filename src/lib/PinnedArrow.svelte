59cf8a1388420bc627c42c94625908c47ef7d535
noradefitero <nora@defitero.com>
2026-09-04 00:38:32 +0200

    feat: add PinnedArrow scroll arrow component
    Fixed-position arrow that translates up and rotates 180° with a
    GSAP/ScrollTrigger scrub tied to the hero section. Clicking jumps
    between hero and the next pinned section via the shared
    ScrollSmoother instance.

---
+++ src/lib/PinnedArrow.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { gsap } from "gsap";
+	import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+	import LargeArrow from "../assets/large_arrow.svelte";
+	import { scroll } from "../shared.svelte";
+    import { HTML } from "@threlte/extras"
+
+	interface Props {
+		to: HTMLElement;
+		tm_trigger: HTMLElement;
+	}
+
+	const { to, tm_trigger }: Props = $props();
+
+	let arrowElement: HTMLButtonElement;
+
+	onMount(() => {
+		gsap.registerPlugin(ScrollTrigger);
+
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: tm_trigger,
+				start: "top top",
+				end: "+=100%",
+				scrub: 1
+			}
+		});
+
+		tl.to(arrowElement, {
+			y: "-20svh",
+			duration: 2,
+			ease: "none"
+		})
+		.to(arrowElement, {
+			rotation: 180,
+			duration: 2,
+			ease: "power2.inOut"
+		})
+	});
+
+	const handleClick = () => {
+		if (scroll.smoother) {
+			if (scroll.scrollTop <= tm_trigger.offsetHeight) {
+				gsap.to(scroll.smoother, {
+					scrollTop: to.offsetTop,
+					duration: 2,
+					ease: "power2.inOut"
+				});
+			}
+			else
+			{
+				gsap.to(scroll.smoother, {
+					scrollTop: 0,
+					duration: 2,
+					ease: "power2.inOut"
+				});
+			}
+		}
+	}
+</script>
+
+<button
+	bind:this={arrowElement}
+	onclick={handleClick}
+	class="fixed top-1/3 right-2 md:right-auto md:left-12 z-50 cursor-pointer"
+	style="translate: 0 -33.3333%;"
+>
+	<LargeArrow />
+</button>
