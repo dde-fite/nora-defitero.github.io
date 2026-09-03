4a00e24588688e2b12af3851b681faf79b6ea568
noradefitero <nora@defitero.com>
2026-09-04 00:38:29 +0200

    feat: add Header component
    Fixed top navigation with a glass nav-pill (Portfolio / About Me /
    Contact) and two circular glass social buttons (GitHub, LinkedIn).

---
+++ src/lib/Header.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import GlassFilter from "./glass/GlassFilter.svelte";
+	import GlassButton from "./glass/GlassButton.svelte"
+	import Github from "../assets/github.svelte";
+	import Linkedin from "../assets/linkedin.svelte";
+</script>
+
+<header class="fixed top-4 flex gap-6 justify-between items-center px-10 z-100">
+	<GlassFilter radiusPercent={100} >
+		<nav class="flex gap-6 justify-end">
+			<button>
+				<span class="text-xl text-primary font-extralight">Portfolio</span>
+			</button>
+			<button>
+				<span class="text-xl text-primary font-extralight">About Me</span>
+			</button>
+			<button>
+				<span class="text-xl text-primary font-extralight">Contact</span>
+			</button>
+		</nav>
+	</GlassFilter>
+	<div class="flex items-center gap-3">
+		<GlassButton type="circle" width={48} height={48}>
+			<Github />
+		</GlassButton>
+		<GlassButton type="circle" width={48} height={48}>
+			<Linkedin />
+		</GlassButton>
+	</div>
+</header>
